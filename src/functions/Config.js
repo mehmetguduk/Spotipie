e4a738303f836860d8bb69f032c75b47aea2ad81
mehmetguduk <mehmetguduk98@gmail.com>
2022-06-06 18:25:03 +0300

    master

--- src/functions/Config.js
+++ src/functions/Config.js
@@ -1,6 +1,6 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 export const clientId = "f52e59dc98cd451582cf23aed4e94e82";
-export const redirectUri = "http://localhost:3000/";
+export const redirectUri = "https://spotify-tracker-mg.netlify.app/";
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
